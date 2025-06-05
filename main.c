97da8feb04fb46c28d52aba81aad35d4921e1f30
2022057656 <bbongvong@hanyang.ac.kr>
2025-06-05 12:18:03 +0900

    1st

--- main.c
+++ main.c
@@ -3,6 +3,120 @@
 #include "init.h"
 #include <stdio.h>
 
+int direct = 0;
+int phase = 0;
+int ncount = 0;
+
+typedef struct {
+    int x;
+    int y;
+} Coordinate;
+
+Coordinate direct_offset[4] = {
+    {0, 1},   // north
+    {-1, 0},  // west
+    {0, -1},  // south
+    {1, 0},   // east
+};
+
+typedef struct Node Node;
+
+typedef struct Direction {
+    int directNumber;     
+    Node* node;            
+    int visited;          
+} Direction;
+
+typedef struct Node {
+    int x;
+    int y;
+    int id;                  
+    int direction_count;      
+    Direction directions[4]; 
+} Node;
+
+Node* now_node;
+
+#define MAX_NODES 50
+Node node_pool[MAX_NODES];
+int node_pool_index = 0;
+
+
+Node* create_node() {
+    if (node_pool_index >= MAX_NODES) {
+        printf("Error: Maximum number of nodes reached\n");
+        return NULL;
+    }
+    Node* node = &node_pool[node_pool_index];
+    initialize_node(node);
+    return node;
+}
+
+
+void initialize_node(Node* node) {
+    node->x = 0;
+    node->y = 0;
+    node->id = node_pool_index++;
+    node->direction_count = 0;
+    int i;
+    for (i = 0; i < 4; i++) {
+        node->directions[i].directNumber = i;
+        node->directions[i].node = NULL;
+        node->directions[i].visited = 0;
+    }
+}
+
+void add_direction(Node* src, int direction_index, Node* dest, int reverse_direction) {
+    if (src->direction_count < 4) {
+        src->directions[direction_index].node = dest;
+        src->directions[direction_index].visited = 1;
+        src->direction_count++;
+    }
+
+    if (dest->direction_count < 4) {
+        dest->directions[reverse_direction].node = src;
+        dest->directions[reverse_direction].visited = 1;
+        dest->direction_count++;
+    }
+}
+
+Node* find_or_create_node(int direction_index, Node* current) {
+    if (current->directions[direction_index].visited == 0) {
+        Node* new_node;
+        if(!check_next(current->x + direct_offset[direction_index].x,
+                current->y + direct_offset[direction_index].y))
+        {
+            new_node = create_node();
+        }
+        else{
+            new_node = &node_pool[0];
+        }
+        int reverse_direction = (direction_index + 2) % 4;
+        new_node->x = current->x + direct_offset[direction_index].x;
+        new_node->y = current->y + direct_offset[direction_index].y;
+        add_direction(current, direction_index, new_node, reverse_direction);
+
+        return new_node;
+    }
+}
+
+Node* Get_Dest(){
+    int x = now_node->x,y = now_node->y,i;
+    Node* cur;
+    while(1)
+    {
+        x += direct_offset[direct].x;
+        y += direct_offset[direct].y;
+        for(i=0;i<node_pool_index;i++){
+            cur = &node_pool[i];
+            printf("curx: %d, cury : %d, x : %d, y : %d\n",cur->x,cur->y,x,y);
+            if(x == cur->x && y == cur->y){
+                return cur;
+            }
+        }
+    }
+}
+
 void main(void)
 {
     Clock_Init48MHz();
@@ -14,7 +128,7 @@ void main(void)
     TA3_N_IRQHandler();
     while (1)
     {
-        //IR¼¾¼­ Å°±â
+        //IRï¿½ï¿½ï¿½ï¿½ Å°ï¿½ï¿½
         P5->OUT |= 0x08;
         P9->OUT |= 0x04;
         P7->DIR = 0xFF;
@@ -32,7 +146,7 @@ void main(void)
         sensor7 = P7->IN & 0x02;
         sensor8 = P7->IN & 0x01;
 
-        //°¡¿îµ¥ -> Á÷Áø
+        //ï¿½ï¿½ï¿½îµ¥ -> ï¿½ï¿½ï¿½ï¿½
         if(sensor4 && sensor5){
             front();
             systick_wait1s();
@@ -42,7 +156,7 @@ void main(void)
             systick_wait1s();
         }
 
-        //IR¼¾¼­ ²ô±â
+        //IRï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
         P5->OUT &= ~0x08;
         P9->OUT &= ~0x04;
         Clock_Delay1ms(10);
